7b27372c4250a19c990b8555d6b41900d8732863
Tyler V <verhovectyler@gmail.com>
2022-04-09 23:02:39 -0600

    Baskets now spawn and can be controlled

--- Apple Picker Prototype/Assets/Scenes/Apple.cs
+++ Apple Picker Prototype/Assets/Scenes/Apple.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Apple : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public static float     bottomY = -20f;
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
-    }
+        if ( transform.position.y < bottomY ) {
+            Destroy( this.gameObject );
+        }
+
+    }//Update
+
 }
