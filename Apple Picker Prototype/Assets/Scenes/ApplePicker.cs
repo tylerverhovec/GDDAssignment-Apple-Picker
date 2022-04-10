acc642f1e54d92cd83a9ecf52ac1e939570298d3
Tyler V <verhovectyler@gmail.com>
2022-04-10 01:17:00 -0600

    Go back to title screen on loss

--- Apple Picker Prototype/Assets/Scenes/ApplePicker.cs
+++ Apple Picker Prototype/Assets/Scenes/ApplePicker.cs
@@ -50,7 +50,7 @@ public class ApplePicker : MonoBehaviour
 
         // If there are no Baskets left, restart the game
         if ( basketList.Count == 0 ) {
-            SceneManager.LoadScene( "_Scene_0" );
+            SceneManager.LoadScene( "_TitleScreen" );
         }
 
     }//AppleDestroyed
